05e7f13a913bd2bb57a5e9e0d38178483424455d
Constantine Rafikov <2constantine@protonmail.com>
2023-09-06 23:24:05 -0300

    Project first version: script, sample file and readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# file-tree-generator
-A simple project scaffolding script that generates file and folder structure from a tree structure drawn with ASCII symbols
+# 🌲 file-tree-generator
+
+The simplest way to generate file and folder structure from a tree drawn with ASCII symbols.
+
+Tags: project scaffolding, tree, file, folder, structure, generator, ASCII, symbols, bash, shell, script
+
+```bash
+sudo ./file-tree.sh
+```
+
+It creates a directory and file structure based on the input format you provided in a **./file-tree.sh** file.
+
+Example of **./file-tree.sh** file:
+
+```text
+some-app/
+|-- public/
+|   |-- index.html
+|-- src/
+|   |-- components/
+|   |   |-- App.vue
+|   |   |-- List.vue
+|   |   |-- SubmissionForm.vue
+|   |   |-- SuccessMessage.vue
+|   |-- main.js
+|   |-- sendEmail.js
+|-- README.md
+|-- vue.config.js
+```
---
+++ file-tree.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Check for root privileges
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root. Please use sudo."
+    exit 1
+fi
+
+# Hard-coded file name
+input_file="sample.txt"  # Modify the path if needed
+
+# Function to create a directory and files based on the input tree structure
+create_files() {
+    local path=$1
+    local input_file=$2
+
+    while IFS= read -r line || [[ -n $line ]]; do
+        # Ignore empty lines or lines starting with #
+        [[ "$line" =~ ^[[:space:]]*$ || "$line" =~ ^[[:space:]]*# ]] && continue
+
+        # Count leading spaces to determine the level
+        local level=0
+        local temp_line="$line"
+        while [[ "$temp_line" == "  "* || "$temp_line" == "|   "* || "$temp_line" == "|" ]]; do
+            temp_line="${temp_line#  }"
+            temp_line="${temp_line#|   }"
+            temp_line="${temp_line#|}"
+            ((level++))
+        done
+
+        # Remove leading symbols indicating the tree structure
+        line="${line/|-- /}"
+        line="${line/|   /}"
+        line="${line/|/}"
+        line="${line/-- /}"
+
+        # Check if the line ends with .<extension>
+        if [[ $line == *"/"* ]]; then
+            # It's a directory
+            local directory_name=$(echo "$line" | sed 's/^[[:space:]]*//')
+            mkdir -p -- "$path/$directory_name"
+        else
+            # It's a file
+            local file_name=$(echo "$line" | tr -d '[:space:]')
+            # Check if it has an extension
+            if [[ "$file_name" != *.* ]]; then
+                file_name="$file_name.txt"
+            fi
+            touch -- "$path/$file_name"
+        fi
+    done < "$input_file"
+}
+
+# Use the current directory as the root directory
+root_directory="."
+mkdir -p "$root_directory"
+
+# Create the file and folder structure based on the hard-coded file name
+create_files "$root_directory" "$input_file"
